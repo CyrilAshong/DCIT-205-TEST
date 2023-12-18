46225b538313d37998e233098ebe50c924dc30e4
CyrilAshong <ashongcyril2704@gmail.com>
2023-12-18 09:02:18 +0000

    register patients

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const Product = require('./models/patients')
+
+app.use(express.json())
 
 
-app.get('/', (req, res) => {
-    res.send('Hello')
+app.get('/patients', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
 })
 
 mongoose.
