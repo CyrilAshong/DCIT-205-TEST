ac276e7214cba533150ec6c59ca6bac6c24d5db7
CyrilAshong <ashongcyril2704@gmail.com>
2023-12-18 09:28:07 +0000

    encounter and patientsvitals

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const Product = require('./models/patients')
+const Patientvitals = require('./models/patientvitals')
 
-app.use(express.json())
+
+app.use(express.json());
 
 
 app.get('/patients', async(req, res) => {
@@ -15,7 +17,15 @@ app.get('/patients', async(req, res) => {
         res.status(500).json({message: error.message})
     }
 })
-
+app.post('/Patientvitals', async(req, res) => {
+    try {
+        const patientvitals = await Patientvitals.create(req.body)
+        res.status(200).json()
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+})
 mongoose.
 connect('mongodb+srv://ashongcyril2704:Wisdom(2704)@cluster0.egbtmp5.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(() =>{
